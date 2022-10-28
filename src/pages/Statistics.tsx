0e7ba4c496268a62d12ef59fc4caa161354cad74
Daniel Pitfield <danielpitfield1@gmail.com>
2022-10-28 05:38:27 +0100

    Driver's championship winning years (end-to-end)

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,12 +1,35 @@
 import { trpc } from "../utils/trpc";
 
+// TODO: Forward the data as a prop to BarChart component
 const Statistics = () => {
-  const { data } = trpc.statistics.getWorldChampions.useQuery();
+  const { data: champions } = trpc.statistics.getWorldChampions.useQuery();
 
-  return data?.map((driver) => {
-    const driverName = `${driver.givenName} ${driver.familyName}`;
-    return <div key={driverName}>{driverName}</div>;
-  });
+  return (
+    <div className="wrapper">
+      {champions?.map((driver) => {
+        // First name and surname
+        const fullName = `${driver.givenName} ${driver.familyName}`;
+        // Get the seasons (championships they won)
+        const { data: winningYears } =
+          trpc.statistics.getDriverWinningYears.useQuery({
+            driverName: driver.familyName,
+          });
+
+        return (
+          <div key={fullName} className="driver-wrapper">
+            <div className="driver-title">{fullName}</div>
+            {winningYears?.map((year) => {
+              return (
+                <div key={year.season} className="driver-winning-year">
+                  {year.season}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Statistics;
--- src/server/trpc/router/statistics.ts
+++ src/server/trpc/router/statistics.ts
@@ -11,6 +11,11 @@ type DriverInfo = {
   nationality: string;
 };
 
+type SeasonInfo = {
+  season: string;
+  url: string;
+};
+
 export const statisticsRouter = router({
   getWorldChampions: publicProcedure.query(async () => {
     const response = await fetch(
@@ -20,4 +25,17 @@ export const statisticsRouter = router({
     const drivers: DriverInfo[] = await data.MRData.DriverTable.Drivers;
     return drivers;
   }),
+
+  getDriverWinningYears: publicProcedure
+    // Must be atleast 1 character, also trim whitespace
+    .input(z.object({ driverName: z.string().min(1).trim() }))
+    .query(async ({ input }) => {
+      const response = await fetch(
+        `http://ergast.com/api/f1/drivers/${input?.driverName}/driverStandings/1/seasons.json?limit=20`
+      );
+      const data = await response.json();
+      const winningSeasons: SeasonInfo[] = await data.MRData.SeasonTable
+        .Seasons;
+      return winningSeasons;
+    }),
 });
