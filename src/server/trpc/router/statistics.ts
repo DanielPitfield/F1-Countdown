0be3461bf235f9bca1e2f859f44d74fa360164ad
Daniel Pitfield <danielpitfield1@gmail.com>
2022-10-28 09:02:30 +0100

    Remove duplicate/copied procedure

--- src/server/trpc/router/statistics.ts
+++ src/server/trpc/router/statistics.ts
@@ -46,34 +46,4 @@ export const statisticsRouter = router({
       })
     );
   }),
-
-  getRaceWinners: publicProcedure.query(async () => {
-    // Get all the drivers that have won a world championship
-    const response = await fetch(
-      "http://ergast.com/api/f1/driverStandings/1/drivers.json?limit=50"
-    );
-    const data = await response.json();
-    const worldChampions: DriverInfo[] = await data.MRData.DriverTable.Drivers;
-
-    return await Promise.all(
-      // For each driver
-      worldChampions.map(async (worldChampion) => {
-        // First name and surname
-        const fullName = `${worldChampion.givenName} ${worldChampion.familyName}`;
-
-        // Get the years they won the championship
-        const response = await fetch(
-          `http://ergast.com/api/f1/drivers/${worldChampion.driverId}/driverStandings/1/seasons.json?limit=20`
-        );
-        const data = await response.json();
-        const winningYears: SeasonInfo[] = await data.MRData.SeasonTable
-          .Seasons;
-
-        return {
-          name: fullName,
-          winningYears: winningYears.map((yearInfo) => yearInfo.season),
-        };
-      })
-    );
-  }),
 });
