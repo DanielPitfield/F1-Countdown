8fa2458287884b6390434a0d5dc17a793ae89b53
Daniel Pitfield <danielpitfield1@gmail.com>
2022-11-02 04:30:50 +0000

    Build time SSG

--- src/pages/teamProfiles/[team].tsx
+++ src/pages/teamProfiles/[team].tsx
@@ -1,53 +1,115 @@
-import { useRouter } from "next/router";
-import React, { useState } from "react";
 import { trpc } from "../../utils/trpc";
 import styles from "../../styles/teamProfile.module.scss";
+import { DriverInfo } from "../../server/trpc/router/driver";
+import { SeasonInfo } from "../../server/trpc/router/statistics";
+import { TeamInfo } from "../../server/trpc/router/team";
+import { GetStaticPaths, GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import { createProxySSGHelpers } from "@trpc/react-query/ssg";
+import { appRouter } from "../../server/trpc/router/_app";
+import superjson from "superjson";
+import { prisma } from "../../server/db/client";
+import { REVALDATION_PERIOD } from "../../utils/limits";
 
-const TeamProfile = () => {
-  const router = useRouter();
+type TeamProfile = {
+  generalInformation: TeamInfo | undefined;
+  currentDrivers: DriverInfo[] | undefined;
+  polePositions: number;
+  raceWins: number;
+  fastestLaps: number;
+  worldChampionships: SeasonInfo[] | undefined;
+};
 
-  const [teamName, setTeamName] = useState<string>("");
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    // TODO: Build time SSG
 
-  React.useEffect(() => {
-    if (router.isReady) {
-      const { team: teamParam } = router.query;
+    /*
+    If the dynamic parameter can be provided below in paths[],
+    The team profile page will be called at build time (the page will load very fast!),
+    Otherwise, it will be called before initial render (due to using fallback: blocking),
+    Therefore, try and save team names in database using Prisma,
+    findMany() method of Prisma context
+    */
 
-      if (!teamParam) {
-        return;
-      }
+    paths: [{ params: { team: "ferrari" } }, { params: { team: "red_bull" } }],
+    fallback: "blocking",
+  };
+};
 
-      setTeamName(teamParam as string);
-    }
-  }, [router.isReady, router.query]);
+export const getStaticProps = async (
+  context: GetStaticPropsContext<{ team: string }>
+) => {
+  // Helper function
+  const ssg = await createProxySSGHelpers({
+    router: appRouter,
+    ctx: {prisma},
+    transformer: superjson, // optional - adds superjson serialization
+  });
 
-  // TODO: Ideally this would be state, but this is a hook which can't be called conditionally or within an useEffect()
+  // The dynamic parameter of the route
+  const team = context.params?.team as string;
+
+  // Pre-fetching data (so that it is immediately available)
+  await ssg.team.getInfo.prefetch({ teamID: team });
+  await ssg.team.getDrivers.prefetch({
+    teamID: team,
+    isReturnOnlyCurrentDrivers: true,
+  });
+  await ssg.team.getPolePositions.prefetch({
+    teamID: team,
+    isReturnOnlyTotalNum: true,
+  });
+  await ssg.team.getRaceWins.prefetch({
+    teamID: team,
+    isReturnOnlyTotalNum: true,
+  });
+  await ssg.team.getFastestLaps.prefetch({
+    teamID: team,
+    isReturnOnlyTotalNum: true,
+  });
+  await ssg.team.getWorldChampionshipWinningYears.prefetch({
+    teamID: team,
+  });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      team,
+    },
+    revalidate: REVALDATION_PERIOD,
+  };
+};
+
+const TeamProfile = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const {team} = props;
+
   const { data: generalInformation } = trpc.team.getInfo.useQuery({
-    teamID: teamName,
+    teamID: team,
   });
 
   const { data: currentDrivers } = trpc.team.getDrivers.useQuery({
-    teamID: teamName,
+    teamID: team,
     isReturnOnlyCurrentDrivers: true,
   });
 
   const { data: polePositions } = trpc.team.getPolePositions.useQuery({
-    teamID: teamName,
+    teamID: team,
     isReturnOnlyTotalNum: true,
   });
 
   const { data: raceWins } = trpc.team.getRaceWins.useQuery({
-    teamID: teamName,
+    teamID: team,
     isReturnOnlyTotalNum: true,
   });
 
   const { data: fastestLaps } = trpc.team.getFastestLaps.useQuery({
-    teamID: teamName,
+    teamID: team,
     isReturnOnlyTotalNum: true,
   });
 
   const { data: worldChampionships } =
     trpc.team.getWorldChampionshipWinningYears.useQuery({
-      teamID: teamName,
+      teamID: team,
     });
 
   return (
@@ -58,7 +120,9 @@ const TeamProfile = () => {
       </div>
 
       <span className={styles.currentDrivers}>
-        {currentDrivers?.map(driver => `${driver.givenName} ${driver.familyName}`).join(", ")}
+        {currentDrivers
+          ?.map((driver) => `${driver.givenName} ${driver.familyName}`)
+          .join(", ")}
       </span>
 
       <div className={styles.resultsInformation}>
--- src/utils/limits.ts
+++ src/utils/limits.ts
@@ -1,6 +1,18 @@
 // If no limit is specified, the default limit is 30
 
 // Ergast F1 API - the maximum number of results returned by a query
-export const MAX_LIMIT = 1000; 
+export const MAX_LIMIT = 1000;
 // There may be reserve drivers
 export const CURRENT_DRIVER_LIMIT = 5;
+
+const SECONDS_IN_DAY = 60 * 60 * 24;
+
+/*
+If a page is kept open which fetches data
+How often should the query be revalidated
+*/ 
+export const REVALDATION_PERIOD = SECONDS_IN_DAY;
+
+
+
+
