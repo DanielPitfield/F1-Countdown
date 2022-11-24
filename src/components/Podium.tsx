77fc3ce57b000901c6e4b1367aab8c005f1d028e
Daniel Pitfield <danielpitfield1@gmail.com>
2022-11-24 03:32:20 +0000

    Podium - Links

--- src/components/Podium.tsx
+++ src/components/Podium.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { DriverRaceResult, RaceInfo } from "../server/trpc/router/race";
 import { getDriverName } from "../utils/getDriverName";
@@ -20,14 +21,26 @@ export const Podium = (props: PodiumProps) => {
     .slice(0, NUM_PODIUM_STEPS);
 
   return (
+    // TODO: Podium Steps CSS, different heights, second place on left, first place in middle, third place on right
     <div>
       <strong>Podium</strong>
       {podiumOrder.map((podiumStep, index) => {
         return (
           <div key={index}>
-            {<span>{getDriverName(podiumStep?.Driver)}</span>}
+            <Link
+              key={podiumStep?.Driver.driverId}
+              href={`/driverProfiles/${podiumStep?.Driver.driverId}`}
+            >
+              {getDriverName(podiumStep?.Driver)}
+            </Link>
+
             {props.showTeams && (
-              <span>{podiumStep?.Constructor.name ?? ""}</span>
+              <Link
+                key={podiumStep?.Constructor.constructorId}
+                href={`/teamProfiles/${podiumStep?.Constructor.constructorId}`}
+              >
+                {podiumStep?.Constructor.name ?? ""}
+              </Link>
             )}
             {props.showTimes && <span>{podiumStep?.Time.time ?? ""}</span>}
           </div>
