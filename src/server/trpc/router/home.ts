d1961b9368565dd2e978e05edaaa1a4212d00a0b
Daniel Pitfield <danielpitfield1@gmail.com>
2023-01-08 01:43:39 +0000

    call procedures of season router

--- src/server/trpc/router/home.ts
+++ src/server/trpc/router/home.ts
@@ -2,20 +2,21 @@ import { router, publicProcedure } from "../trpc";
 import { z } from "zod";
 import { getCurrentYear } from "../../../utils/getCurrentYear";
 import { GrandPrixWeekend } from "./grandPrix";
-import { trpc } from "../../../utils/trpc";
 import { getGrandPrixWeekendSessions } from "../../../utils/getGrandPrixWeekendSessions";
+import { seasonRouter } from "./season";
+import { prisma } from "../../db/client";
 
 export const homeRouter = router({
-  // TODO: Not sure if you can use procedures within other procedures
   getUpcomingGrandPrixWeekend: publicProcedure.query(
     async (): Promise<GrandPrixWeekend | undefined> => {
+      // Call procedures from seasonRouter, from the server, using this caller
+      const caller = seasonRouter.createCaller({ prisma });
       // Try getting the next event within the schedule of the current/most recent season
-      const { data: currentSeasonSchedule } = trpc.season.getSchedule.useQuery({
+      const currentSeasonSchedule = await caller.getSchedule({
         seasonID: "current",
       });
-
       // If not (i.e. end of season), try getting the next event within the schedule of the current year
-      const { data: currentYearSchedule } = trpc.season.getSchedule.useQuery({
+      const currentYearSchedule = await caller.getSchedule({
         seasonID: getCurrentYear().toString(),
       });
 
@@ -29,9 +30,9 @@ export const homeRouter = router({
 
 // Find the first event which has a session in the future
 function getNextEventInYear(
-  schedule: GrandPrixWeekend[] | undefined
+  schedule: GrandPrixWeekend[]
 ): GrandPrixWeekend | undefined {
-  return schedule?.find((grandPrixWeekend) => {
+  return schedule.find((grandPrixWeekend) => {
     const sessions = getGrandPrixWeekendSessions(grandPrixWeekend);
     const sessionDates: Date[] = sessions.map((session) => session.date);
 
