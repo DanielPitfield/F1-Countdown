f72fec2b36314f455a068991d947f35ade7f9e5a
Daniel Pitfield <danielpitfield1@gmail.com>
2023-01-12 00:56:51 +0000

    navbar styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,18 +17,16 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <div className={styles.containerOuter}>
-          <div className={styles.containerInner}>
-            <h1 className={styles.title}>
-              <span className={styles.titlePink}>F1 Dashboard</span>
-            </h1>
+      <div className={styles.containerOuter}>
+        <div className={styles.containerInner}>
+          <h1 className={styles.title}>
+            <span className={styles.titlePink}>F1 Dashboard</span>
+          </h1>
 
-            <UpcomingWeekendSummary />
-            <TwitterProfileTimeline profileName="F1" isDarkMode />
-          </div>
+          <UpcomingWeekendSummary />
+          <TwitterProfileTimeline profileName="F1" isDarkMode />
         </div>
-      </main>
+      </div>
     </>
   );
 };
