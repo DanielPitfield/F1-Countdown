c43069cbf64b739352329dfd1bb272dd31f96708
Daniel Pitfield <danielpitfield1@gmail.com>
2023-01-12 08:49:51 +0000

    add times to session dates

--- src/utils/getGrandPrixWeekendSessions.ts
+++ src/utils/getGrandPrixWeekendSessions.ts
@@ -2,6 +2,17 @@ import { GrandPrixWeekend } from "../server/trpc/router/grandPrix";
 
 export type WeekendSession = { name: string; date: Date | null };
 
+// Get the date object of a session (using both date and time)
+function getFullSessionDate(
+  session: { date: string; time: string } | undefined
+): Date | null {
+  if (!session) {
+    return null;
+  }
+
+  return new Date(`${session.date} ${session.time}`);
+}
+
 export function getGrandPrixWeekendSessions(
   weekend: GrandPrixWeekend | undefined
 ): WeekendSession[] {
@@ -9,33 +20,28 @@ export function getGrandPrixWeekendSessions(
     return [];
   }
 
-  // TODO: Add times to date objects (e.g weekend.FirstPractice.time)
   return [
     {
       name: "Free Practice 1",
-      date: weekend.FirstPractice?.date
-        ? new Date(weekend.FirstPractice.date)
-        : null,
+      date: getFullSessionDate(weekend.FirstPractice),
     },
     {
       name: "Free Practice 2",
-      date: weekend.SecondPractice?.date
-        ? new Date(weekend.SecondPractice.date)
-        : null,
+      date: getFullSessionDate(weekend.SecondPractice)
+
     },
     {
       name: "Free Practice 3",
-      date: weekend.ThirdPractice?.date
-        ? new Date(weekend.ThirdPractice.date)
-        : null,
+      date: getFullSessionDate(weekend.ThirdPractice),
     },
     {
       name: "Qualifying",
-      date: weekend.Qualifying?.date ? new Date(weekend.Qualifying.date) : null,
+      date: getFullSessionDate(weekend.Qualifying),
     },
     {
       name: "Race",
-      date: new Date(weekend.date),
+      // The date and time of the race is not a nested object
+      date: new Date(`${weekend.date} ${weekend.time}`),
     },
   ];
 }
