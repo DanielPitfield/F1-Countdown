f91e06a2ff973be68063318bfb401654890a6cda
Daniel Pitfield <danielpitfield1@gmail.com>
2024-12-05 19:34:43 +0000

    Fix max limit

--- src/data/limits.ts
+++ src/data/limits.ts
@@ -1,4 +1,4 @@
 // If no limit is specified, the default limit is 30
 
 // Ergast F1 API - the maximum number of results returned by a query
-export const MAX_LIMIT = 1000;
+export const MAX_LIMIT = 100;
