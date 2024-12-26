282b8549fec850a0bff59a8490a1e199ab60e91d
Daniel Pitfield <danielpitfield1@gmail.com>
2024-12-26 09:58:27 +0000

    Only home page

--- src/utils/getDriverName.ts
+++
@@ -1,5 +0,0 @@
-import { Driver } from "./types/Driver";
-
-export function getDriverName(driver: Driver | undefined): string {
-  return driver ? `${driver?.givenName} ${driver?.familyName}` : "";
-}
--- src/utils/types/GrandPrix.ts
+++ src/utils/types/GrandPrix.ts
@@ -1,6 +1,4 @@
 import { Circuit } from "./Circuit";
-import { Driver } from "./Driver";
-import { Team } from "./Team";
 
 // The schedule and information for a Grand Prix event/weekend
 export type GrandPrixWeekend = {
@@ -17,59 +15,6 @@ export type GrandPrixWeekend = {
   Qualifying: { date: string; time: string };
   // Some grand prix weekends will have a sprint race instead of a third practice session
   Sprint?: { date: string; time: string };
-};
-
-// The qualifying result for an individual driver
-export type DriverQualifyingResult = {
-  number: string;
-  position: string;
-  Driver: Driver;
-  Constructor: Team;
-  Q1: string;
-  Q2: string;
-  Q3: string;
-};
-
-export type Qualifying = {
-  season: string;
-  round: string;
-  url: string;
-  raceName: string;
-  Circuit: Circuit;
-  date: string;
-  time?: string;
-  QualifyingResults: DriverQualifyingResult[];
-};
-
-// The race result for an individual driver
-export type DriverRaceResult = {
-  number: string;
-  position: string;
-  positionText: string;
-  points: string;
-  Driver: Driver;
-  Constructor: Team;
-  grid: string;
-  laps: string;
-  status: string;
-  Time?: { millis: string; time: string };
-  FastestLap?: {
-    rank: string;
-    lap: string;
-    Time: { time: string };
-    AverageSpeed: { units: string; speed: string };
-  };
-};
-
-export type Race = {
-  season: string;
-  round: string;
-  url: string;
-  raceName: string;
-  Circuit: Circuit;
-  date: string;
-  time?: string;
-  Results: DriverRaceResult[];
 };
 
 export type SessionName =
