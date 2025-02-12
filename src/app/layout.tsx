f9cceef4f8fc226d71b6c07e511c04a89fb8828e
Daniel Pitfield <danielpitfield1@gmail.com>
2025-02-12 07:47:34 +0000

    Remove nav, profiles, other pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,10 @@
 import "./globals.scss";
 
-import Navbar from "../components/Navbar";
-import { getCurrentCircuits } from "../utils/serverActions/home/getCurrentCircuits";
-import { getCurrentDrivers } from "../utils/serverActions/home/getCurrentDrivers";
-import { getCurrentTeams } from "../utils/serverActions/home/getCurrentTeams";
-
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  // Cached but may wish to the use the upcoming "use cache" directive (just to make sure)
-  const [currentDrivers, currentTeams, currentCircuits] = await Promise.all([
-    getCurrentDrivers(),
-    getCurrentTeams(),
-    getCurrentCircuits(),
-  ]);
-
   return (
     <html lang="en">
       <body>
-        <>
-          <Navbar currentDrivers={currentDrivers} currentTeams={currentTeams} currentCircuits={currentCircuits} />
-          <main>{children}</main>
-        </>
+        <main>{children}</main>
       </body>
     </html>
   );
